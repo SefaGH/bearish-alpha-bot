951115da3cce6bdf5694e13cdd89164107512af4
SefaGH <79759593+SefaGH@users.noreply.github.com>
2025-10-23 21:03:38 +0300

    Fix shebang alignment in devcontainer setup script

--- .devcontainer/scripts/set-python-default.sh
+++ .devcontainer/scripts/set-python-default.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-PYTHON_BIN="/usr/local/python/current/bin/python"
-PYTHON3_BIN="/usr/local/python/current/bin/python3"
+readonly PYTHON_BIN="/usr/local/python/current/bin/python"
+readonly PYTHON3_BIN="/usr/local/python/current/bin/python3"
 
 sudo update-alternatives --install /usr/bin/python python "${PYTHON_BIN}" 1
 sudo update-alternatives --install /usr/bin/python3 python3 "${PYTHON3_BIN}" 1
