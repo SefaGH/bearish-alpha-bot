5f4f6028aede71935ab718ae5bcbadedee1247e0
SefaGH <79759593+SefaGH@users.noreply.github.com>
2025-11-14 01:13:54 +0300

    Update analyze_model_explainability.py

--- scripts/analyze_model_explainability.py
+++ scripts/analyze_model_explainability.py
@@ -264,9 +264,9 @@ def main():
     X_train, X_test, y_train, y_test = train_test_split(
         X_full, 
         y_full, 
-        test_size=0.20, #
+        test_size=0.20, 
         random_state=42, 
-        stratify=y_full #
+        shuffle=False # <<< DOĞRU YÖNTEM (Zaman sırasını korur)
     )
     
     # <<< BAŞLANGIÇ: YENİ ÖLÇEKLEME (SCALING) ADIMI >>>
